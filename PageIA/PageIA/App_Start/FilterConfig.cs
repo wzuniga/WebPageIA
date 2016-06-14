48a3b15b9d832a34d042cc97e6a6e8f1a350e589
wzuniga <wzuniga@episunsa.edu.pe>
2016-06-13 23:37:12 -0500

    Starting proyect

---
+++ PageIA/PageIA/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PageIA
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ PageIA/PageIA/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PageIA.MvcApplication" Language="C#" %>
